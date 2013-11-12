907d13afd3a55b4ec441135c4cc0427fd13fe67b
Zach Tomaszewski <zach.tomaszewski@gmail.com>
2013-11-11 16:24:14 -0800

    Basic delivery in place * Still in progress

---
+++ delivery/config.py
@@ -0,0 +1,45 @@
+# Local configuration for delivery.py
+
+import os.path
+import subprocess
+
+# System directory from where all the game files are being served from.
+# Absolute or relative from cgi-bin.
+FILES_DIR = '/zacht/httpd/htdocs/'
+
+# Fully-qualified domain name of server from which game files are being served
+# No slash at end (so can append port if needed)
+FILES_URL_SERVER = 'localhost'
+
+# Directory path part of URL pointing to files location.
+FILES_URL_DIR = '/'
+
+
+# The directory to store saved files and logs into.  Path given should
+# be either relative to this script or absolute from drive root. 
+DATA_DIR = '/zacht/httpd/skaldData'
+
+# Name of the counter file that is source of userIDs
+COUNTER_FILE = os.path.join(DATA_DIR, 'counter.txt')
+# Name of the counter file that is source of which experimental group 
+# the next user gets assigned to.
+EXP_GROUP = os.path.join(DATA_DIR, 'expgroup.txt')
+
+# Name of the file to store timestamps in within each user's directory.
+# UserID will be prepended to this
+TIMES_FILE = 'times.txt'
+
+# Should begin with http://...
+BACKGROUND_SURVEY_URL = 'http://www.surveygizmo.com/s3/439469/Demeter-Evaluation-Consent-Background'
+RESPONSE_SURVEY_URL = 'http://www.surveygizmo.com/s3/439486/Demeter-Evaluation-Player-Response'
+
+# Location of TADS interpreter capable of acting as a server
+# May include any TADS options, after which the game file will be appended.
+TADS = '/usr/local/bin/frob --interface plain -N00 --no-pause --webhost ' + FILES_URL_SERVER
+#TADS = '/usr/bin/wine ' + DATA_DIR + '/t3run.exe -plain -cs ASCII -ns0 -webhost ' + FILES_URL_SERVER
+
+# Where to read/write to the void
+DEVNULL = subprocess.DEVNULL
+
+# Lowest port usable by users.  Should be divisible by 10 and must be >= 10000.
+MIN_USER_PORT = 30000
--- delivery/delivery.py
+++ delivery/delivery.py
@@ -3,16 +3,16 @@
 ## delivery.py
 ## 
 ## Serves up the TADS servers to run UI evaluation.  
-## Also generates a userID, records game transcripts sent home, 
+## Also generates a user, records game transcripts sent home, 
 ## and keeps track of that sort of thing.
 ## 
 ## Basic process:
-## no userID - generate a new userID (logged as "start")
+## no user - generate a new user (logged as "start")
 ##             and assign to experimental group
 ##             and direct to background survey
-## (userID, no stage) - error
-## userID, stage 1 - Serve first game for given exp group
-## userID, stage 2 - Serve second game
+## (user, no stage) - error
+## user, stage 1 - Serve first game for given exp group
+## user, stage 2 - Serve second game
 ## 
 ## Author: Zach Tomaszewski
 ## Created: 06 Dec 2010 (as demeter.py)
@@ -26,141 +26,74 @@ import os
 import subprocess
 import time
 import random
+import time
 
-# Where all the game files are being served from
-FILES_DIR = '../htdocs/skald'
-
-# Fully-qualified domain name of server from which game files are being served
-# No slash at end (so can append port if needed)
-FILES_URL_SERVER = 'http://demo.zach.tomaszewski.name'
-
-# Directory path part of URL pointing to files location.
-FILES_URL_DIR = '/'
-
-
-# The directory to store saved files and logs into.  Path given should
-# be either relative to this script or absolute from drive root. 
-DATA_DIR = os.path.normpath('../skaldData')
-
-# Name of the counter file that is source of userIDs
-COUNTER_FILE = os.path.join(DATA_DIR, 'counter.txt')
-# Name of the counter file that is source of which experimental group 
-# the next user gets assigned to.
-EXP_GROUP = os.path.join(DATA_DIR, 'expgroup.txt')
-
-# Name of the file to store timestamps in within each user's directory.
-# UserID will be prepended to this
-TIMES_FILE = 'times.txt'
-
-# Should begin with http://...
-BACKGROUND_SURVEY_URL = 'http://www.surveygizmo.com/s3/439469/Demeter-Evaluation-Consent-Background'
-RESPONSE_SURVEY_URL = 'http://www.surveygizmo.com/s3/439486/Demeter-Evaluation-Player-Response'
-
-# Location of TADS interpreter capable of acting as a server
-# May include any TADS options, after which the game file will be appended.
-TADS = '/usr/local/bin/frob -i plain -N 0'
-
-FATE = 'fate.t3'
-QUEEN = 'queen.t3'
-
-# Games: [experimental group][stage]
-GAMES = [[FATE + ' web',   QUEEN + ' skald'],
-         [FATE + ' skald', QUEEN + ' web']]
-
+from config import *
 
 def main():
-#    runStudy()
-#    serveGames()
-    serveGame(FATE, 'skald', generateUserID())
-  
-
-def serveGames():
-    """
-    Only serves up the two games with no user tracking.
-    """
-    body = """
-<body>
-<h3>Available games:</h3>
-<ul>
-<li>Captain Fate: 
-  text UI / Skald UI
-<li>Queen's Heart: 
-  text UI / Skald UI  
-</body>"""
-    printHtmlPage("Available Games", body)
-
-
-def serveGame(game, mode, port):
-    """
-    Starts the given game file in the given mode ('web' or 'skald')
-    on the given port.
-    """
-    cmd = ' '.join([TADS, os.path.join(DATA_DIR, game), mode, port])
-    
-    print("Content-Type: text/html")
-    print("Status: 307")
-    print("Location: {}:{}{}".format(FILES_URL_SERVER, port, FILES_URL_DIR))
-    print()
-
-    # save output to file
-    outputfile = "{}-{}.{}.output".format(port, game, mode)
-    outputfile = os.path.join(DATA_DIR, outputfile)
-
-    # spawn separate process
-    subprocess.Popen(cmd, shell=True, close_fds=True,
-                    stdin=open('/dev/null'),
-                    stdout=open(outputfile, 'w'),
-                    stderr=subprocess.STDOUT)
-    # print(cmd)
- 
-    
+    runStudy()
+      
 
 def runStudy():
-  """
-  Runs in research study mode where each user is assigned an ID and routed
-  through surveys.
-  """
-  form = cgi.FieldStorage()
-  
-  #get user ID
-  userID = form.getfirst('c', None) or form.getfirst('userID', None)
-  if not userID:
-    userID = generateUserID()
-    logTime(userID, "Start")
-    group = assignToGroup()
-    logTime(userID, "Grp=" + str(group))
-    welcomePage(userID, group)    
-    return
-  elif not re.compile(r"\d\d\d$").match(userID):
-    #bad userID
-    returnStatus(400, "Bad username format")
-    return
-
-  #get any passed stage of survey so far (if not uploading)
-  if not filename:
-    stage = form.getfirst('s', 0) or form.getfirst('stage', 0)
-    if stage and stage == '1' or stage == '2':
-      #a valid stage to be in
-      stage = int(stage)
-    else:
-      returnStatus(400, "Given stage argument (" + str(stage) + ") is not supported.")
+    """
+    Runs in research study mode where each user is assigned an ID and routed
+    through surveys.
+    """
+    form = cgi.FieldStorage()
+    
+    #get user ID
+    user = form.getfirst('c', None) or form.getfirst('user', None)
+    if not user:
+      user = generateUser()
+      logTime(user, "Start")
+      group = assignToGroup()
+      logTime(user, "Grp=" + str(group))
+  #    welcomePage(user, group)
+      serveGames(user)
       return
 
-  #now, do what we came to do...
-  #first, log what we're doing
-  logTime(userID, stage)
+    elif not re.compile(r"\d{1,5}$").match(user) or int(user) < MIN_USER_PORT:
+      #bad user
+      returnStatus(400, "Bad username format")
+      return
+
+    assert user  # have a valid user given from here one
+
+    game = form.getfirst('game', None)
+    if game:  # direct game request
+        serveGame(game, user)
+        return
   
-  #serve the correct game for this user
-  game = getGame(userID, stage)
-  if game:
-    generateJNLP(userID, game)
-    serveApplet(userID, game, stage)
-  else:
-    returnStatus(400, "Something wrong: No such game to play.<br>\
-      Check that you are really a valid user and that you submitted which stage you are on.")
+
+    #get any passed stage of survey so far (if not uploading)
+    """if not filename:
+      stage = form.getfirst('s', 0) or form.getfirst('stage', 0)
+      if stage and stage == '1' or stage == '2':
+        #a valid stage to be in
+        stage = int(stage)
+      else:
+        returnStatus(400, "Given stage argument (" + str(stage) + ") is not supported.")
+        return
+
+    #now, do what we came to do...
+    #first, log what we're doing
+    logTime(user, stage)
+
+    #serve the correct game for this user
+    game = getGame(user, stage)
+    if game:
+      generateJNLP(user, game)
+      serveApplet(user, game, stage)
+    else:
+      returnStatus(400, "Something wrong: No such game to play.<br>\
+        Check that you are really a valid user and that you submitted which stage you are on.")
+      return
+
+    """
+    returnStatus(400, "Incomplete request or invalid server state.")
     return
-
-
+    
+  
 def assignToGroup():
     """
     Opens EXP_GROUP, gets the number there, then flips the number (b/w 0 and 1)
@@ -202,35 +135,8 @@ def assignToGroup():
 
     return counter
 
-
-def generateJNLP(userID, game):
-  """
-  Copies the original demeter.jnlp, appending the userID and -stage to 
-  filename (before extension) and updating the userID contained within the 
-  file itself.
-  """
-  #get original contents
-  jnlp = open(os.path.join(DEMETER_DIR, 'demeter.jnlp'), 'r')
-  contents = jnlp.read()
-  jnlp.close()
-
-  #replace userID 
-  found = re.compile(USER_ID_PRE + '(\d\d\d)').search(contents)
-  contents = contents.replace(found.group(0), USER_ID_PRE + userID)
-  #point to game version
-  found = re.compile(GAME_ID_PRE).search(contents)
-  contents = contents.replace(found.group(0), GAME_ID_PRE + str(game))
-  
-  # prints out a separate jnlp file in case we get another request from a 
-  # different user before the first (on a slow connection, perhaps) actually
-  # reads the jnlp file
-  userJnlp = open(os.path.join(DEMETER_DIR, 'demeter' + userID + '-' + str(game) + '.jnlp'), 'w')
-  userJnlp.write(contents)
-  jnlp.close()
-
-
     
-def generateUserID():
+def generateUser():
     """
     Opens COUNTER_FILE, increments the number there, and returns the new
     value as a 3-digit string.  COUNTER_FILE should contain an integer 
@@ -264,19 +170,20 @@ def generateUserID():
     lockFile.close()
     os.remove(lockFile.name)
 
-    return '49%03i' % counter  #49... 3 digit integer
+    counter = MIN_USER_PORT + (counter * 10)
+    return '%05i' % counter
 
 
-def getGame(userID, stage):
+def getGame(user, stage):
   """
-  Opens the times file for the given userID and finds the first timestamp for
+  Opens the times file for the given user and finds the first timestamp for
   "Grp=#:", where # is the number of the grp.  Then uses this number, stage,
   and VERSION_BASE to return the number of the game this user should currently
   be playing.
   
   Returns 0 if no Grp= assigned yet or if stage is not 1 or 2.
   """
-  userTimes = open(os.path.join(DATA_DIR, userID + TIMES_FILE), 'r')
+  userTimes = open(os.path.join(DATA_DIR, user + TIMES_FILE), 'r')
   for line in userTimes:
     grp = re.compile("Grp=(\d):").match(line)
     if grp:
@@ -288,11 +195,11 @@ def getGame(userID, stage):
   return 0
   
   
-def logTime(userID, stage=0):
+def logTime(user, stage=0):
   """
   Dumps the current time into the given user's TIMES_FILE.
   """
-  userTimes = open(os.path.join(DATA_DIR, userID + TIMES_FILE), 'a')
+  userTimes = open(os.path.join(DATA_DIR, user + TIMES_FILE), 'a')
   userTimes.write(str(stage))
   userTimes.write(": ")
   userTimes.write(time.ctime())  #human-readable
@@ -300,125 +207,29 @@ def logTime(userID, stage=0):
   userTimes.write(str(int(time.time()))) #seconds since epoch
   userTimes.write('\n')
   userTimes.close()
+   
 
-  
-  
-  
-  
-def serveApplet(userID, game, stage):
-  """
-  Generates an HTML page that launches the demeter.jnlp
-  """
-  jnlpFile = DEMETER_HREF + 'demeter' + userID + '-' + str(game) + ".jnlp"
-  
-  print("Content-Type: text/html")
-  print()
-  #print HTML
-  print("""
-<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.0 Transitional//EN">
-<html>
-<head>
-<meta http-equiv="Content-Type" content="text/html;charset=utf-8" >
-<title>Demeter (Zag Edition)</title>
-</head>
+def printHtmlPage(title, body, status=None):
+    """
+    Prints the content-type header (with optional Status line), then the HTML 
+    document.  Uses a standard head containing the given title.  Then includes
+    the given body text, which must start and end with <body></body>.
+    """
+    print("Content-Type: text/html")
+    if status:
+       print("Status: " + str(status))
 
-<body style="font-family: sans-serif;  margin: 1em;
-  """)
-  if stage == 1:
-    print('background-color: #fffbda;"')
-  elif stage == 2:
-    print('background-color: #dae0e0;"')
-  
-  print(""">
-    <table>
-    <tr><td width="450" style="border: 1px solid black;">
-    <applet code='org.p2c2e.zag.AppletMain'
-      archive='""" + DEMETER_HREF + """demeter.jar'
-      jnlp_href='""" + jnlpFile + """'
-      width="450" height="300">
-      Your browswer either does not have Java installed or does not support the APPLET tag.
-      You can visit <a href="http://www.java.com/">java.com</a> to get the current Java plugin.
-      <param name="gameFile" value='../demeter/Demeter""" + str(game) + """.ulx' />
-      <param name="saveToFilePath" value="../cgi-bin/demeter.py" />
-      <param name="userID" value='""" + str(userID) + """' />  
-    </applet>
-    
-    <td style="padding: 1em;">
-    <h2>Game session """ + str(stage) + """ of 2</h2>    
-    """)
-  if stage == 1:
-    print("""
-    <p>
-    The <i>Demeter</i> game should open in another window, beginning with 
-    a request from Java to allow it to run.
-    <p>
-    Your game input is being anonymously recorded.  Since this is your
-    first time playing, the game will start with a short
-    tutorial to get your oriented.
-    <p>
-    Play one time through the tutorial and the game that follows.
-    """)
-  elif stage == 2:
-    print("""
-    <p>
-    Welcome back!  This version of <i>Demeter</i> has slightly different
-    settings than the first version you played.  As before, the game
-    will open in a new window. Your game input is still being
-    anonymously recorded.  Since this is your second time, we'll skip
-    the tutorial.
-    """)
-  print('<p><br><b>Once you finish the game, <a href="' + RESPONSE_SURVEY_URL + \
-    "?userID=" + str(userID) + "&s=" + str(stage) + '">click here to continue with the study.</a></b>')
-  print("""    
-    <p><br>
-    If you encounter a technical problem with the game and need to 
-    restart this session, you can:</b>
-    <ul>
-    <li>Hit refresh in your browser to reload this page.
-    <li>If that doesn't work, try to <a href='""" + jnlpFile + """'>open the game
-    with Java Web Start</a>.
-    <li>See the FAQs below for more specific help.
-    </ul>
-    </tr></table>
-    <br>
-    
-    <div style="float: right; border: 1px dotted black; padding: 1em; margin-right: 2em;">
-    You are here:<br>    
-    <strike><i>1. Background Survey</i></strike><br>
-  """)
-  if stage == 1:
-    print("""
-    <b>2. Game Session 1 of 2</b><br>
-    <i>3. Response Survey</i><br>    
-    <i>4. Game Session 2 of 2</i><br>
-    <i>5. Response Survey</i><br>
-    """)
-  elif stage == 2:
-    print("""
-    <strike><i>2. Game Session 1 of 2</i></strike><br>
-    <strike><i>3. Response Survey</i></strike><br>    
-    <b>4. Game Session 2 of 2</b><br>
-    <i>5. Response Survey</i><br>
-    """)
-  print("""  
-    </div>
-    
-    <h4>FAQs</h4>
-    <ul>
-    <li><a href="http://www2.hawaii.edu/~ztomasze/argax/games/demeter/faqs.html#warning" 
-    target="_blank">Why do I get a warning from Java when the applet first loads?</a>
-    <li><a href="http://www2.hawaii.edu/~ztomasze/argax/games/demeter/faqs.html#signature" 
-    target="_blank">Why does this application need a signature in the first place?</a>
-    <li><a href="http://www2.hawaii.edu/~ztomasze/argax/games/demeter/faqs.html#no_start" 
-    target="_blank">I do not get any pop-up window at all.</a>
-    <li><a href="http://www2.hawaii.edu/~ztomasze/argax/games/demeter/faqs.html#cancel"
-    target="_blank">I hit Cancel in the initial pop-up window from Java and now the game won't start.</a>
-    </ul>
-</body>
-</html>
-""")  
+    print()
+    #print HTML
+    print("""<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.0 Transitional//EN">
+  <html>
+  <head>
+  <meta http-equiv="Content-Type" content="text/html;charset=utf-8" >
+  <title>""" + title + """</title>
+  </head>""" + body + """</html>""")
 
-  
+
+#TODO: refactor to use printHtmlPage
 def returnStatus(status, detail):
   """
   Returns the given status code, with short webpage, to the sender.
@@ -452,45 +263,104 @@ def returnStatus(status, detail):
 </body>
 </html> 
   """)  
-  
 
-def welcomePage(userID, group):
-  """
-  Alpha testing splash page
-  """
-  body = """
+
+def serveGame(game, user):
+    """
+    Starts the given game file on a port derived from the given int(user).
+    That is, user should be a string that converts to an int.  Will then
+    add 1 to 4 to that base port number depending on the game requested:
+    fate-webui: +1, fate-skald: +2, queen-webui: +3, queen-skald: +4
+    
+    """
+    if not re.compile(r'(fate|queen)-(skald|webui)\.t3').match(game):
+        returnStatus(400, "Unsupported game requested.")
+        return
+
+    port = int(user) + 1
+    if game.startswith('queen'):
+        port += 2
+    webui = game.endswith('-webui.t3')
+    if webui:
+        tads_opts = '--webhost ' + FILES_URL_SERVER
+    else:
+        port += 1
+        tads_opts = ''
+        url = "http://{}:{}{}".format(FILES_URL_SERVER, port, FILES_URL_DIR)
+        
+        
+    cmd = ' '.join([TADS, tads_opts, os.path.join(DATA_DIR, game), str(port)])
+
+    # save output to file
+    outputfile = "{}-{}.output".format(port, game)
+    outputfile = os.path.join(DATA_DIR, outputfile)
+
+    # spawn separate process
+    subprocess.Popen(cmd, shell=True, close_fds=True,
+                    stdin=open(DEVNULL),
+                    stdout=open(outputfile, 'w'),
+                    stderr=open(outputfile + '.err', 'w'))
+    #print(cmd)
+    time.sleep(1)  # give it a second to start
+
+    if webui:
+        try:
+            with open(outputfile, 'r') as f:
+                for i in range(1): f.readline()  # skip over
+                url = f.readline()
+                #print(url)
+                url = url[len('connectWebUI:'):] # remove prefix
+                if not url.startswith('http:'):
+                    returnStatus(500, 'Could not start game')
+                    return
+        except IOError:
+            returnStatus(500, 'Could not start game--no link file generated')
+            return
+    
+    print("Content-Type: text/html")
+    print("Status: 307")
+    print("Location: " + url)
+    print()
+
+
+def serveGames(user=MIN_USER_PORT):
+    """
+    Only serves up the four games with no user tracking.
+    If no user given, uses MIN_USER_PORT, which is cause conflict for more than
+    one concurrent user.  
+    """
+    user = int(user)
+        
+    body = """
+<body>
+<h3>Available games:</h3>
+<ul>
+<li>Captain Fate: 
+  <a href="?user={0}&game=fate-webui.t3">text UI</a> / 
+  <a href="?user={0}&game=fate-skald.t3">Skald UI</a>
+<li>Queen's Heart: 
+  <a href="?user={0}&game=queen-webui.t3">text UI</a> / 
+  <a href="?user={0}&game=queen-skald.t3">Skald UI</a>
+</body>""".format(user)
+    printHtmlPage("Available Games", body) 
+
+
+def welcomePage(user, group):
+    """
+    Alpha testing splash page
+    """
+    body = """
 <body>
 <h3>Welcome</h3>
 <p>
-Thanks for being an beta tester!  You are currently anonymous user """ + str(userID) + """.
+Thanks for being an beta tester!  You are currently anonymous user """ + str(user) + """.
 <p>
-Please <a href='""" + BACKGROUND_SURVEY_URL + '?userID=' + str(userID) + '&group=' + str(group) + \
+Please <a href='""" + BACKGROUND_SURVEY_URL + '?user=' + str(user) + '&group=' + str(group) + \
 """'><b>click here to begin</b></a>.
 </p>
 </body>
-  """
-  printHtmlPage('Welcome!', body)
-
-def printHtmlPage(title, body, status=None):
-  """
-  Prints the content-type header (with optional Status line), then the HTML 
-  document.  Uses a standard head containing the given title.  Then includes
-  the given body text, which must start and end with <body></body>.
-  """
-  print("Content-Type: text/html")
-  if status:
-    print("Status: " + str(status))
-    
-  print()
-  #print HTML
-  print("""<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.0 Transitional//EN">
-<html>
-<head>
-<meta http-equiv="Content-Type" content="text/html;charset=utf-8" >
-<title>""" + title + """</title>
-</head>""" + body + """</html>""")
-  
-
+    """
+    printHtmlPage('Welcome!', body)
   
   
 if __name__ == "__main__":
